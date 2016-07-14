cfdfaf95794fe69c3130b76217b48fd9a0d73422
YaroslavGnatyuk <gnat2k@gmail.com>
2016-07-14 09:57:02 +0300

    I added config for Spring and Hibernate.
    You should input your personal server,user and password in hibernate.cfg.xml .
    exemple:
    db.url = jdbc:mysql://127.0.0.1:3306/your_db
    db.username = user
    db.password = pass
    
    I added simple test with simple query.
    I added dependency "spring-test" in pom.xml  for testing spring.

--- src/test/java/TestSimpleDBQuery.java
+++ src/test/java/TestSimpleDBQuery.java
@@ -1,5 +1,30 @@
-/**
- * Created by yaroslav on 7/13/16.
- */
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import ua.skillsup.gelius.SpringConfig;
+
+import java.math.BigInteger;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = SpringConfig.class)
+
 public class TestSimpleDBQuery {
+    @Autowired
+    SessionFactory sessionFactory;
+
+    @Test
+    public void simpleQuery(){
+        Session session = sessionFactory.openSession();     // Test returns true if we have only three records in table CLIENTS
+        BigInteger actual = (BigInteger) session.createSQLQuery("SELECT COUNT(*) FROM CLIENTS").uniqueResult();
+        BigInteger expected = BigInteger.valueOf(3);
+
+        assertEquals(expected, actual);
+    }
+
 }
