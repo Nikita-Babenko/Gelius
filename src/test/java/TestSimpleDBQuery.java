a8ba31a50b5b18837cc28b8b4cd0d761179c6138
YaroslavGnatyuk <gnat2k@gmail.com>
2016-07-23 09:35:41 +0300

    I added log4j.properties I changed version of junit fro 4.1 to 4.9

--- src/test/java/TestSimpleDBQuery.java
+++ src/test/java/TestSimpleDBQuery.java
@@ -1,8 +1,10 @@
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import ua.skillsup.gelius.configs.HibernateConfig;
 
 
@@ -10,7 +12,7 @@ import java.math.BigInteger;
 
 import static org.junit.Assert.assertEquals;
 
-//@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = HibernateConfig.class)
 
 public class TestSimpleDBQuery {
@@ -20,7 +22,7 @@ public class TestSimpleDBQuery {
     @Test
     public void simpleQuery(){
         Session session = sessionFactory.openSession();     // Test returns true if we have only three records in table CLIENTS
-        BigInteger actual = (BigInteger) session.createSQLQuery("SELECT COUNT(*) FROM CLIENTS").uniqueResult();
+        BigInteger actual = (BigInteger) session.createSQLQuery("SELECT 1").uniqueResult();
         BigInteger expected = BigInteger.valueOf(3);
 
         assertEquals(expected, actual);
