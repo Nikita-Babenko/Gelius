c4c49304b1fb07c3c8a707aa5ac8eed30d47d9e7
Yan-Brav <yan.braverman37@gmail.com>
2016-07-26 18:56:17 +0300

    #1104 Filtering implemented with Hibernate criteria API (multi filtering)

--- src/main/java/ua/skillsup/gelius/dao/ProductFilter.java
+++ src/main/java/ua/skillsup/gelius/dao/ProductFilter.java
@@ -1,11 +1,12 @@
 package ua.skillsup.gelius.dao;
 
 import ua.skillsup.gelius.dto.ProductDto;
+import ua.skillsup.gelius.dto.ProductsSearchFilter;
 
 import java.util.List;
 
 public interface ProductFilter {
-    List<ProductDto> filterByClient(String client);
+    /*List<ProductDto> filterByClient(String client);
     ProductDto filterByProductName(String name);
     List<ProductDto> filterByProductType(String type);
     List<ProductDto> filterByInnerLength(int length);
@@ -14,5 +15,6 @@ public interface ProductFilter {
     List<ProductDto> filterByGrade(String grade);
     List<ProductDto> filterByProfile(String profile);
     List<ProductDto> filterByColour(String colour);
-    List<ProductDto> filterByPrint(String print);
+    List<ProductDto> filterByPrint(String print);*/
+    List<ProductDto> findByFilter(ProductsSearchFilter filter);
 }
--- src/main/java/ua/skillsup/gelius/dao/impl/ProductFilterImpl.java
+++ src/main/java/ua/skillsup/gelius/dao/impl/ProductFilterImpl.java
@@ -1,11 +1,14 @@
 package ua.skillsup.gelius.dao.impl;
 
+import org.hibernate.Criteria;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import ua.skillsup.gelius.dao.ProductFilter;
 import ua.skillsup.gelius.dto.ProductDto;
+import ua.skillsup.gelius.dto.ProductsSearchFilter;
 import ua.skillsup.gelius.model.entities.Product;
 
 import java.util.ArrayList;
@@ -13,128 +16,104 @@ import java.util.List;
 
 import static ua.skillsup.gelius.converters.EntityDtoConverter.convert;
 
+
 @Repository
 @Transactional
 public class ProductFilterImpl implements ProductFilter{
     @Autowired
     private SessionFactory sessionFactory;
 
-    @Override
-    public List<ProductDto> filterByClient(String client) {
-        List<Product> products = sessionFactory.getCurrentSession().createQuery("from Product").list();
-        List<ProductDto> result = new ArrayList<>();
-        for (Product product : products){
-            if (product.getClient().getCompanyName().equals(client)) {
-                result.add(convert(product));
-            }
-        }
-        return result;
-    }
 
     @Override
-    public ProductDto filterByProductName(String name) {
-        List<Product> products = sessionFactory.getCurrentSession().createQuery("from Product").list();
-        ProductDto result = null;
-        for (Product product : products){
-            if (product.getProductsName().equals(name)) {
-                result = convert(product);
+    public List<ProductDto> findByFilter(ProductsSearchFilter filter) {
+        Criteria criteria = sessionFactory.getCurrentSession().createCriteria(Product.class);
+        List<Product> products = criteria.list();
+        if (!filter.isEmpty()) {
+            List<String> clients = filter.getClients();
+            for (Product product : products){
+                for (String client : clients){
+                    if (product.getClient().getCompanyName().equals(client)){
+                        criteria.add(Restrictions.eq("client", product.getClient()));
+                    }
+                }
+            }
+            List<String> names = filter.getNames();
+            for (Product product : products){
+                for (String name : names){
+                    if (product.getProductsName().equals(name)){
+                        criteria.add(Restrictions.eq("product_name", product.getProductsName()));
+                    }
+                }
+            }
+            List<String> types = filter.getTypes();
+            for (Product product : products){
+                for (String type : types){
+                    if (product.getProductsType().equals(type)){
+                        criteria.add(Restrictions.eq("product_type", product.getProductsType()));
+                    }
+                }
+            }
+            List<Integer> lengths = filter.getLengths();
+            for (Product product : products){
+                for (Integer length : lengths){
+                    if (product.getInnerLength().equals(length)){
+                        criteria.add(Restrictions.eq("inner_length", product.getInnerLength()));
+                    }
+                }
+            }
+            List<Integer> widths = filter.getWidths();
+            for (Product product : products){
+                for (Integer width : widths){
+                    if (product.getInnerWidth().equals(width)){
+                        criteria.add(Restrictions.eq("inner_width", product.getInnerWidth()));
+                    }
+                }
+            }
+            List<Integer> heights = filter.getHeights();
+            for (Product product : products){
+                for (Integer height : heights){
+                    if (product.getInnerHeight().equals(height)){
+                        criteria.add(Restrictions.eq("inner_height", product.getInnerHeight()));
+                    }
+                }
+            }
+            List<String> grades = filter.getGrades();
+            for (Product product : products){
+                for (String grade : grades){
+                    if (product.getGrade().equals(grade)){
+                        criteria.add(Restrictions.eq("grade", product.getGrade()));
+                    }
+                }
+            }
+            List<String> profiles = filter.getProfiles();
+            for (Product product : products){
+                for (String profile : profiles){
+                    if (product.getProfile().equals(profile)){
+                        criteria.add(Restrictions.eq("profile", product.getProfile()));
+                    }
+                }
+            }
+            List<String> colours = filter.getColours();
+            for (Product product : products){
+                for (String colour : colours){
+                    if (product.getColour().equals(colour)){
+                        criteria.add(Restrictions.eq("colour", product.getColour()));
+                    }
+                }
+            }
+            List<String> prints = filter.getPrints();
+            for (Product product : products){
+                for (String print : prints){
+                    if (product.getPrint().equals(print)){
+                        criteria.add(Restrictions.eq("print", product.getPrint()));
+                    }
+                }
             }
         }
-        return result;
-    }
-
-    @Override
-    public List<ProductDto> filterByProductType(String type) {
-        List<Product> products = sessionFactory.getCurrentSession().createQuery("from Product").list();
-        List<ProductDto> result = new ArrayList<>();
-        for (Product product : products){
-            if (product.getProductsType().equals(type)) {
-                result.add(convert(product));
-            }
-        }
-        return result;
-    }
-
-    @Override
-    public List<ProductDto> filterByInnerLength(int length) {
-        List<Product> products = sessionFactory.getCurrentSession().createQuery("from Product").list();
-        List<ProductDto> result = new ArrayList<>();
-        for (Product product : products){
-            if (product.getInnerLength().equals(length)) {
-                result.add(convert(product));
-            }
-        }
-        return result;
-    }
-
-    @Override
-    public List<ProductDto> filterByInnerWidth(int width) {
-        List<Product> products = sessionFactory.getCurrentSession().createQuery("from Product").list();
-        List<ProductDto> result = new ArrayList<>();
-        for (Product product : products){
-            if (product.getInnerWidth().equals(width)) {
-                result.add(convert(product));
-            }
-        }
-        return result;
-    }
-
-    @Override
-    public List<ProductDto> filterByInnerHeight(int height) {
-        List<Product> products = sessionFactory.getCurrentSession().createQuery("from Product").list();
-        List<ProductDto> result = new ArrayList<>();
-        for (Product product : products){
-            if (product.getInnerHeight().equals(height)) {
-                result.add(convert(product));
-            }
-        }
-        return result;
-    }
-
-    @Override
-    public List<ProductDto> filterByGrade(String grade) {
-        List<Product> products = sessionFactory.getCurrentSession().createQuery("from Product").list();
-        List<ProductDto> result = new ArrayList<>();
-        for (Product product : products){
-            if (product.getGrade().equals(grade)) {
-                result.add(convert(product));
-            }
-        }
-        return result;
-    }
-
-    @Override
-    public List<ProductDto> filterByProfile(String profile) {
-        List<Product> products = sessionFactory.getCurrentSession().createQuery("from Product").list();
-        List<ProductDto> result = new ArrayList<>();
-        for (Product product : products){
-            if (product.getProfile().equals(profile)) {
-                result.add(convert(product));
-            }
-        }
-        return result;
-    }
-
-    @Override
-    public List<ProductDto> filterByColour(String colour) {
-        List<Product> products = sessionFactory.getCurrentSession().createQuery("from Product").list();
-        List<ProductDto> result = new ArrayList<>();
-        for (Product product : products){
-            if (product.getColour().equals(colour)) {
-                result.add(convert(product));
-            }
-        }
-        return result;
-    }
-
-    @Override
-    public List<ProductDto> filterByPrint(String print) {
-        List<Product> products = sessionFactory.getCurrentSession().createQuery("from Product").list();
-        List<ProductDto> result = new ArrayList<>();
-        for (Product product : products){
-            if (product.getPrint().equals(print)) {
-                result.add(convert(product));
-            }
+        products = criteria.list();
+        List<ProductDto> result = new ArrayList<ProductDto>(products.size());
+        for (Product product : products) {
+            result.add(convert(product));
         }
         return result;
     }
