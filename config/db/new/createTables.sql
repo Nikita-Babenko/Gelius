40045a7e45d823a57d1d5a743ea14c52b5b26fbb
AndrexGold <andrexgold@gmail.com>
2016-08-12 16:11:32 -0700

    #1170 Renew DB structure

---
+++ config/db/new/createTables.sql
@@ -0,0 +1,211 @@
+/* Клиент */
+CREATE TABLE client(
+  client_id BIGINT AUTO_INCREMENT,
+  address VARCHAR(200),
+  company_name VARCHAR(50) NOT NULL,
+  description VARCHAR(255),
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  prhone_number VARCHAR(20) NOT NULL,
+
+  PRIMARY KEY (client_id)
+);
+
+/* Тип изделия */
+CREATE TABLE product_type(
+  product_type_id BIGINT AUTO_INCREMENT,
+  product_type VARCHAR(50),
+
+  PRIMARY KEY(product_type_id)
+);
+
+/* Расчетный формат */
+CREATE TABLE format(
+  format_id BIGINT AUTO_INCREMENT,
+  format VARCHAR(50),
+
+  PRIMARY KEY(format_id)
+);
+
+/* Профиль */
+CREATE TABLE profile(
+  profile_id BIGINT AUTO_INCREMENT,
+  profile VARCHAR(50),
+
+  PRIMARY KEY(profile_id)
+);
+
+/* Марка картона */
+CREATE TABLE cardboard_brand(
+  cardboard_brand_id BIGINT AUTO_INCREMENT,
+  cardboard_brand VARCHAR(50),
+
+  PRIMARY KEY (cardboard_brand_id)
+);
+
+/* Целюлозный слой */
+CREATE TABLE cellulose_layer(
+  cellulose_layer_id BIGINT AUTO_INCREMENT,
+  cellulose_layer VARCHAR(50),
+
+  PRIMARY KEY (cellulose_layer_id)
+);
+
+/* Лицевой слой */
+CREATE TABLE face_layer(
+  face_layer_id BIGINT AUTO_INCREMENT,
+  face_layer VARCHAR(50),
+
+  PRIMARY KEY (face_layer_id)
+);
+
+/* Внутренний слой */
+CREATE TABLE inner_layer(
+  inner_layer_id BIGINT AUTO_INCREMENT,
+  inner_layer VARCHAR(50),
+
+  PRIMARY KEY (inner_layer_id)
+);
+
+/* Соединение клапана */
+CREATE TABLE connection_valve(
+  connection_valve_id BIGINT AUTO_INCREMENT,
+  connection_valve VARCHAR(50),
+
+  PRIMARY KEY (connection_valve_id)
+);
+
+/* Технологичность */
+CREATE TABLE workability(
+  workability_id BIGINT AUTO_INCREMENT,
+  service_center VARCHAR(100),
+
+  PRIMARY KEY (workability_id)
+);
+
+/* Способ упаковки */
+CREATE TABLE packing(
+  packing_id BIGINT AUTO_INCREMENT,
+  packing VARCHAR(50),
+
+  PRIMARY KEY (packing_id)
+);
+
+CREATE TABLE pallet(
+  pallet_id BIGINT AUTO_INCREMENT,
+  pallet VARCHAR(50),
+
+  PRIMARY KEY (pallet_id)
+);
+
+/* Поддон */
+CREATE TABLE pallet_placement(
+  pallet_placement_id BIGINT AUTO_INCREMENT,
+  pallet_placement VARCHAR(50),
+
+  PRIMARY KEY (pallet_placement_id)
+);
+
+/* Продукт */
+CREATE TABLE product(
+  product_id BIGINT AUTO_INCREMENT,
+
+  product_number BIGINT NOT NULL,
+  isNew BOOLEAN NOT NULL,
+
+  product_create DATE,
+  product_update DATE,
+  person_prepared VARCHAR(50),
+
+  isUse BOOLEAN,
+  client BIGINT,
+  product_name VARCHAR(200),
+  product_type BIGINT,
+
+  inner_length INT,
+  inner_width INT,
+  inner_height INT,
+  theoretical_square INT,
+  actual_square INT,
+  format BIGINT,
+  profile BIGINT,
+  cardboard_brand BIGINT,
+  cellulose_layer BIGINT,
+  face_layer BIGINT,
+  inner_layer BIGINT,
+  material VARCHAR(200),
+  special_conditions VARCHAR(500),
+
+
+  size_workpiece_length INT,
+  size_workpiece_width INT,
+  number_from_sheet INT,
+  blank_format INT,
+  connection_valve BIGINT,
+  stamp VARCHAR(50),
+  cliche VARCHAR(50),
+  packing BIGINT,
+  number_in_pack INT,
+  number_in_transport_package INT,
+  package_length INT,
+  package_width INT,
+  package_height INT,
+  pallet BIGINT,
+  pallet_placement BIGINT,
+  pallet_rows INT,
+  number_load_car INT,
+  production_format INT,
+
+  PRIMARY KEY (product_id),
+  UNIQUE(product_number, isNew),
+  FOREIGN KEY (client) REFERENCES client(client_id),
+  FOREIGN KEY (product_type) REFERENCES product_type(product_type_id),
+  FOREIGN KEY (format) REFERENCES format(format_id),
+  FOREIGN KEY (profile) REFERENCES profile(profile_id),
+  FOREIGN KEY (cardboard_brand) REFERENCES cardboard_brand(cardboard_brand_id),
+  FOREIGN KEY (cellulose_layer) REFERENCES cellulose_layer(cellulose_layer_id),
+  FOREIGN KEY (face_layer) REFERENCES face_layer(face_layer_id),
+  FOREIGN KEY (inner_layer) REFERENCES inner_layer(inner_layer_id),
+  FOREIGN KEY (connection_valve) REFERENCES connection_valve(connection_valve_id),
+  FOREIGN KEY (packing) REFERENCES packing(packing_id),
+  FOREIGN KEY (pallet) REFERENCES pallet(pallet_id),
+  FOREIGN KEY (pallet_placement) REFERENCES pallet_placement(pallet_placement_id)
+);
+
+/* Печать */
+CREATE TABLE print(
+  print_id BIGINT AUTO_INCREMENT,
+  product_id BIGINT NOT NULL,
+  isNew BOOLEAN NOT NULL,
+  colour VARCHAR(50),
+  name VARCHAR(50),
+  square_seal INT,
+
+  PRIMARY KEY (print_id),
+  FOREIGN KEY (product_id) REFERENCES product(product_id)
+);
+
+/* Примечания */
+CREATE  TABLE workability_notes(
+  workability_notes_id BIGINT AUTO_INCREMENT,
+  product_id BIGINT NOT NULL,
+  isNew BOOLEAN NOT NULL,
+  service_center BIGINT,
+  notes VARCHAR(100),
+
+  PRIMARY KEY (workability_notes_id),
+  FOREIGN KEY (product_id) REFERENCES product(product_id),
+  FOREIGN KEY (service_center) REFERENCES workability(workability_id)
+);
+
+/* Просечки */
+CREATE TABLE perforation(
+  perforation_id BIGINT AUTO_INCREMENT,
+  product_id BIGINT NOT NULL,
+  value INT,
+
+  PRIMARY KEY (perforation_id),
+  FOREIGN KEY (product_id) REFERENCES product(product_id)
+);
+
+COMMIT;
---
+++ config/db/new/dropTables.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS print;
+DROP TABLE IF EXISTS workability_notes;
+DROP TABLE IF EXISTS perforation;
+DROP TABLE IF EXISTS product;
+DROP TABLE IF EXISTS client;
+DROP TABLE IF EXISTS product_type;
+DROP TABLE IF EXISTS format;
+DROP TABLE IF EXISTS profile;
+DROP TABLE IF EXISTS cardboard_brand;
+DROP TABLE IF EXISTS cellulose_layer;
+DROP TABLE IF EXISTS face_layer;
+DROP TABLE IF EXISTS inner_layer;
+DROP TABLE IF EXISTS connection_valve;
+DROP TABLE IF EXISTS workability;
+DROP TABLE IF EXISTS packing;
+DROP TABLE IF EXISTS pallet;
+DROP TABLE IF EXISTS pallet_placement;
+COMMIT;
