34824168491a2dec368228531737ef7ff238ca8e
mchukd <mchukdp@gmail.com>
2016-08-25 17:50:16 +0300

    Fix position of work centers modal on small screens

--- src/main/java/ua/skillsup/gelius/dao/entity/Product.java
+++ src/main/java/ua/skillsup/gelius/dao/entity/Product.java
@@ -10,7 +10,7 @@ import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Entity
-@Table(name = "product")
+@Table(name = "product", uniqueConstraints = {@UniqueConstraint(columnNames = {"product_number", "isNew"})})
 public class Product {
 
     @Id
