80f20842fad3d3b8e71121bcc6200bc2b0a7a920
mchukd <mchukdp@gmail.com>
2016-10-04 01:25:45 +0300

    Upgrade "blank Format" selection

--- src/main/frontend/js/components/newProduct/BlankFormat.js
+++ src/main/frontend/js/components/newProduct/BlankFormat.js
@@ -3,6 +3,21 @@ import EventConstants from "../../constants/Events";
 import NewProductStore from "../../stores/NewProductStore";
 
 class BlankFormat extends React.Component {
+
+    componentWillMount() {
+        NewProductStore.addListener(EventConstants.NEW_PRODUCT_CHANGE_EVENT, this.__loadDefaultValue);
+    }
+
+    componentDidUpdate() {
+        var value = $('#blankFormat option:selected').val();
+        if (value != this.props.workpieceSize.workpieceLength && value != this.props.workpieceSize.workpieceWidth)
+            $('#blankFormat').val('');
+    }
+
+    componentWillUnmount() {
+        NewProductStore.removeListener(EventConstants.NEW_PRODUCT_CHANGE_EVENT, this.__loadDefaultValue);
+    }
+
     render() {
         return (
             <select id="blankFormat">
@@ -15,9 +30,11 @@ class BlankFormat extends React.Component {
         );
     }
 
-    componentDidUpdate() {
-        var value = NewProductStore.getProductProperty("blankFormat");
-        $('#blankFormat').val(value ? value : '');
+    __loadDefaultValue() {
+        if (NewProductStore.isEnableDefaultValues()) {
+            var value = NewProductStore.getProductProperty("blankFormat");
+            $('#blankFormat').val(value ? value : '');
+        }
     }
 
 }
